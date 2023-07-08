1987cdec144210f79c18c955a1717941cbf6f4f5
Ayobami <tnenas07@gmail.com>
2023-07-08 18:01:59 +0100

    first commit

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,3 @@
-// export const server = 'http://localhost:8000/api/v2';
 export const server = 'https://eshop-1l8c.onrender.com/api/v2';
 
-// export const backend_url = 'http://localhost:8000/';
 export const backend_url = 'https://eshop-1l8c.onrender.com/';
